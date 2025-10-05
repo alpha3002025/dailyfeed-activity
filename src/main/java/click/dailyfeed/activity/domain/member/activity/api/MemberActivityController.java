3610284e24ad50eafd20dfb81ad4da930474ccb5
alpha3002025 <alpha300uk@gmail.com>
2025-10-05 12:03:15 +0900

    chore: member-activities 계획

---
+++ src/main/java/click/dailyfeed/activity/domain/member/activity/api/MemberActivityController.java
@@ -0,0 +1,10 @@
+package click.dailyfeed.activity.domain.member.activity.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member-activities")
+public class MemberActivityController {
+
+}
